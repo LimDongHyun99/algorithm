b51c8dbcdb15821058d1728ab9dc1e68e023260e
LimDongHyun99 <108440279+LimDongHyun99@users.noreply.github.com>
2022-12-08 19:01:50 +0900

    [Bronze V] Title: 검증수, Time: 204 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = 0;
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            num = sc.nextInt();
+            sum += num * num;
+        }
+        sc.close();
+        System.out.println(sum%10);
+    }
+}
