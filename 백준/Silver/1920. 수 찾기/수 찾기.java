02bcaaab95a476889542c258b78de25f60687800
LimDongHyun99 <108440279+LimDongHyun99@users.noreply.github.com>
2022-12-15 17:21:48 +0900

    [Silver IV] Title: 수 찾기, Time: 2256 ms, Memory: 191164 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < n; i++)
+            set.add(sc.nextInt());
+        int m = sc.nextInt();
+            for (int j = 0; j < m; j++) {
+                if(set.contains(sc.nextInt()))
+                    System.out.println(1);
+                else System.out.println(0);
+            }
+        }
+    }
