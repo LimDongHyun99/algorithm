0104b236bcf9cbfd3937a33b3ca9afce9492a4d8
LimDongHyun99 <108440279+LimDongHyun99@users.noreply.github.com>
2023-01-16 18:12:17 +0900

    [Bronze IV] Title: !밀비 급일, Time: 124 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while(true) {
+            String str = br.readLine();
+            if (str.equals("END")) break;
+            StringBuilder sb = new StringBuilder(str);
+            String reversedStr = sb.reverse().toString();
+            System.out.println(reversedStr);
+        }
+    }
+}
