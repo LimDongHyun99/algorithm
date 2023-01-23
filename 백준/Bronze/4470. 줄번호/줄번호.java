90b6e13ca6e026f871943735820d795d5cc0a9a7
LimDongHyun99 <108440279+LimDongHyun99@users.noreply.github.com>
2023-01-23 23:23:31 +0900

    [Bronze IV] Title: 줄번호, Time: 232 ms, Memory: 18408 KB -BaekjoonHub

--- 백준/Bronze/4470. 줄번호/줄번호.java
+++ 백준/Bronze/4470. 줄번호/줄번호.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = Integer.parseInt(sc.nextLine());
+        int n = sc.nextInt();
+        sc.nextLine();
         for (int i = 1; i <= n; i++) {
             System.out.println((i) + ". " + sc.nextLine());
         }
